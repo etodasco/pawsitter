eabb7ea2465bf84a021c70d472b18adde6b2548f
Eric Todasco <erictodasco@me.com>
2024-11-19 21:00:09 -0500

    updated reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,14 @@
 class ReservationsController < ApplicationController
+  before_action :set_pet_sitter, except: [:show]
   def show
     @reservation = Reservation.find(params[:id])
     @message = Message.new
   end
+
+  def new
+    @reservation = Reservation.new
+  end
+
   def create
     @reservation = Reservation.new(reservation_params)
     @reservation.restaurant = @restaurant
@@ -15,6 +21,10 @@ class ReservationsController < ApplicationController
   end
 
   private
+
+  def set_pet_sitter
+    @pet_sitter = User.find(params[:pet_sitter_id])
+  end
 
   def reservation_params
     params.require(:reservation).permit(:date)
